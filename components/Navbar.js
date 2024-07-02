24325e1775c4ba32164b890484bf196522ef3d58
StephenTrainor <stephontrainor@gmail.com>
2024-07-02 13:36:59 -0700

    Fixed unknown class error using className instead

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,10 +8,9 @@ export default function Navbar({title}) {
     const { data: session } = useSession();
 
     return (
-        // <header class="flex grow max-h-16 justify-center items-center text-white m-3 sm:m-5">
-        <header class="flex justify-center items-center text-white m-3 sm:m-5">
-            <div class="flex-1 justify-center flex">
-                <div class="mr-auto w-10 sm:w-16 h-10 sm:h-16 relative inline-block">
+        <header className="flex justify-center items-center text-white m-3 sm:m-5">
+            <div className="flex-1 justify-center flex">
+                <div className="mr-auto w-10 sm:w-16 h-10 sm:h-16 relative inline-block">
                     <Image 
                         alt="Circled check mark Icon for the RightRaise website"
                         src="/images/Icon.png"
@@ -22,14 +21,14 @@ export default function Navbar({title}) {
                 </div>
             </div>
 
-            <div class="flex-none md:flex-1 justify-center hidden md:flex">
-                <p class="invisible md:visible text-center md:text-xl lg:text-2xl">{title || "RightRaise"}</p>
+            <div className="flex-none md:flex-1 justify-center hidden md:flex">
+                <p className="invisible md:visible text-center md:text-xl lg:text-2xl">{title || "RightRaise"}</p>
             </div>
 
-            <div class="flex-none md:flex-1 justify-center flex">
-                <nav class="ml-auto list-none inline-block">
-                    <ul class="">
-                        <li class="inline-block p-3 sm:p-4 mr-2 sm:mr-3">
+            <div className="flex-none md:flex-1 justify-center flex">
+                <nav className="ml-auto list-none inline-block">
+                    <ul className="">
+                        <li className="inline-block p-3 sm:p-4 mr-2 sm:mr-3">
                             { (session) ? 
                                 <button onClick={() => signOut()}>Log Out</button> :
                                 <button onClick={() => signIn('google')}>Sign In</button>
