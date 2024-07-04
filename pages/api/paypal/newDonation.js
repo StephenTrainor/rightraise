753740e1f1789703edef5d504324b32616bbe759
StephenTrainor <stephontrainor@gmail.com>
2024-07-03 21:31:17 -0700

    Added api path to update database for new donations

---
+++ pages/api/paypal/newDonation.js
@@ -0,0 +1,25 @@
+import { db } from '../../../firebase';
+import { updateDoc, doc } from 'firebase/firestore';
+
+export default async function handler(req, res) {
+    const { body: { name, amount, message, projectData, projectID } } = req.body;
+
+    if (!name || !amount || !message || !projectData || !projectID) {
+        res.json({"status": 400, "errorMessage": "Incomplete API request", "bodyReceived": req.body});
+        return;
+    }
+
+    projectData.donations.push({
+        amount: amount,
+        message: message,
+        name: name,
+    });
+
+    const projectRef = doc(db, "projects", projectID);
+
+    await updateDoc(projectRef, {
+        donations: projectData.donations,
+    });
+
+    res.json(projectData.donations);
+};
